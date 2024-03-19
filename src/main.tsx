f8bcdd3efa34115f7d720154bd4ed0eaea042e51
Massimo <37376701+max8392@users.noreply.github.com>
2024-03-19 12:44:08 +0100

    14 add list leaned word (#15)
    * add react router
    
    * add drawer + word list learned
    
    * add french translation

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
-import "./utils/i18n"
+import "./utils/i18n";
 import App from "./App.tsx";
 import "./index.css";
 import { store } from "./utils/store.ts";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>,
+    </Provider>
+    ,
   </React.StrictMode>
 );
